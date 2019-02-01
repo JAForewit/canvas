2b22fe32f3b3c097b74d1e722411ba6a50c1548f
Anderson <MDAnderson@samaritan.org>
2019-02-01 18:30:50 -0500

    added vue components

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,16 @@
 
+Vue.component('widget-toolbar', {
+    data: function() {
+        return {
+            showToolbar: false,
+        };
+    },
+    template: '<div><h1 v-bind:class="{ \'open\': showToolbar }" class="Toolbar">Toolbar</h1></div>'
+});
+
 new Vue({
     el: '#app',
     data: {
-        showToolbar: false,
-    },
-    methods: {
-
+        title: 'title',
     }
 });
