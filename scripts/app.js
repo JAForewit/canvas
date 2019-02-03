c576edd1a4ad7460da273a1cd7597a0a07ae91bd
jaforewit <marcda495@gmail.com>
2019-02-03 00:55:47 -0500

    added widget and toolbar vue components

--- scripts/app.js
+++ scripts/app.js
@@ -1,17 +1,37 @@
-// widget manager Vue
 new Vue({
-    el: '#manager',
-    data: {
-        showToolbar: true,
-    },
+    el: '#app',
+    data: {},
+    components: {
+        'toolbar' : {
+            template: `
+            <div v-bind:class="{ 'open': showToolbar }" class="toolbar">
+                <button v-on:click="showToolbar = !showToolbar" style="float: right; margin-right: -100px;">toggle toolbar</button>    
+                <slot></slot>
+            </div>`,
+            props: {},
+            data: function() {
+                return {
+                    showToolbar: true
+                }
+            }
+        },
+        'widget': {
+            template: `
+                <div class='widget'>
+                    <div class='widget-handle'>
+                        <slot></slot>
+                    </div>
+                </div>`,
+            mounted: function() {
+                options = {
+                    grid: 100,
+                    smoothDrag: true,
+                    handle: this.$el.children[0]
+                };
+                new Draggable(this.$el, options);
+            },
+            props: {},
+        }
+    }
+    
 });
-
-// Initialize dragable widget
-element = document.getElementsByClassName('widget')[0];
-handle = document.getElementsByClassName('widget-handle')[0];
-var options = {
-	grid: 100,
-    smoothDrag: true,
-    handle: handle
-};
-var drag = new Draggable(element, options);
