363749a260d6bfabaca5173e26a61cf9838cf118
jaforewit <marcda495@gmail.com>
2019-02-02 19:20:50 -0500

    cleanup

--- scripts/app.js
+++ scripts/app.js
@@ -2,15 +2,16 @@
 new Vue({
     el: '#manager',
     data: {
-        showToolbar: false,
+        showToolbar: true,
     },
 });
 
 // Initialize dragable widget
 element = document.getElementsByClassName('widget')[0];
+handle = document.getElementsByClassName('widget-handle')[0];
 var options = {
 	grid: 100,
-	setCursor: true,
-    smoothDrag: true
+    smoothDrag: true,
+    handle: handle
 };
 var drag = new Draggable(element, options);
