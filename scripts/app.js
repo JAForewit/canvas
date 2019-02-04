895b9313717f0b6a25ba00ba289c2b1ccdde7070
Anderson <MDAnderson@samaritan.org>
2019-02-04 15:26:47 -0500

    starting to build limit functionality for grid-layout component

--- scripts/app.js
+++ scripts/app.js
@@ -2,22 +2,46 @@ Vue.component('grid-layout', {
     props: {
         cols: {
             type: Number,
-            required: true
+            default: 1
+        },
+        handles: {
+            type: Boolean,
+            default: false
         }
+    },
+    data: function() {
+        return {
+            items: []
+        };
     },
     template: `
     <div>
         <slot></slot>
     </div>`,
-    mounted: function() {
-        for (var i=0; i<this.$el.children.length; i++) {
-            element = this.$el.children[i];
-            options = {
-                grid: 100,
-                smoothDrag: true,
-            };
-            new Draggable(element, options);
+    methods: {
+        dragStart: function() {
+            console.log(":drag start");
+        },
+        dragEnd: function() {
+            console.log(":drag end");
         }
+    },
+    mounted: function() {
+        // create list of draggable items
+        for (var i=0; i<this.$el.children.length; i++) {
+            console.log(i);
+            element = this.$el.children[i];
+            options = { 
+                smoothDrag: true,
+                onDragStart: this.dragStart,
+                onDragEnd:  this.dragEnd
+            };
+            if (this.handles) options.handle = element.children[0];
+            drag = new Draggable(element, options);
+            this.items.push(drag);
+        };
+
+        // limit movability of draggable items
     },
 });
 
