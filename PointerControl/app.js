a5398f741097434a17b769c68f42d987a18b6573
Anderson <MDAnderson@samaritan.org>
2019-03-08 11:57:23 -0500

    testing with css only

--- PointerControl/app.js
+++ PointerControl/app.js
@@ -1,4 +1,41 @@
-var el = document.getElementById("wrapper");
+// see: https://medium.com/turo-engineering/ios-mobile-scroll-in-web-react-1d92d910604b
+
+const preventDefault = e => e.preventDefault();
+// When rendering our container
+window.addEventListener('touchmove', preventDefault, {
+   passive: false
+});
+// Remember to clean up when removing it
+window.removeEventListener('touchmove', preventDefault);
+
+
+
+function scrollToPreventBounce(htmlElement) {
+    const {scrollTop, offsetHeight, scrollHeight} = htmlElement;
+  
+    // If at top, bump down 1px
+    if (scrollTop <= 0) {
+      htmlElement.scrollTo(0, 1);
+      return;
+    }
+  
+    // If at bottom, bump up 1px
+    if (scrollTop + offsetHeight >= scrollHeight) {
+      htmlElement.scrollTo(0, scrollHeight - offsetHeight - 1);
+    }
+  }
+  // When rendering the element
+  function afterRender() {
+     htmlElement.addEventListener('touchstart', scrollToPreventBounce);
+  }
+  // Remember to clean-up when removing it
+  function beforeRemove() {
+     htmlElement.removeEventListener('touchstart', scrollToPreventBounce);
+  }
+
+
+
+/*var el = document.getElementById("scrollableBox");
 el.addEventListener("touchstart", handleStart, false);
 el.addEventListener("touchend", handleEnd, false);
 el.addEventListener("touchcancel", handleCancel, false);
@@ -13,7 +50,7 @@ function handleStart(evt) {
 
   for (var i = 0; i < touches.length; i++) {
     ongoingTouches.push(copyTouch(touches[i]));
-    log("start touch " + i);
+    log("start touch " + i + " on " + evt.currentTarget.id);
   }
 }
 
@@ -25,7 +62,7 @@ function handleMove(evt) {
     var idx = ongoingTouchIndexById(touches[i].identifier);
 
     if (idx >= 0) {
-      log("continuing touch " + idx);
+      log("continuing touch " + idx + " on " + evt.currentTarget.id);
       ongoingTouches.splice(idx, 1, copyTouch(touches[i]));  // swap in the new touch record
     } else {
       log("can't figure out which touch to continue");
@@ -42,7 +79,7 @@ function handleEnd(evt) {
 
     if (idx >= 0) {
       ongoingTouches.splice(idx, 1);  // remove it; we're done
-      log("end touch " + idx)
+      log("end touch " + idx + " on " + evt.currentTarget.id)
     } else {
       log("can't figure out which touch to end");
     }
@@ -78,4 +115,4 @@ function ongoingTouchIndexById(idToFind) {
 function log(msg) {
     var p = document.getElementById('log');
     p.innerHTML = msg;
-}
+}*/
