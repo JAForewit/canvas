3cd2b9b2f8ec4fec37dd45e106ce7fb1603449f9
Anderson <MDAnderson@samaritan.org>
2019-03-09 10:56:36 -0500

    na

--- PointerControl/app.js
+++ PointerControl/app.js
@@ -1,59 +1,29 @@
 // Function to disable "pull-to-refresh" effect present in some webviews.
 // Especially Crosswalk 12 and above (Chromium 41+) runtimes.
 
-window.addEventListener('load', function() {
-    var lastTouchY = 0 ;
-    var maybePreventPullToRefresh = false ;
+window.addEventListener('load', function () {
+    var maybePreventPullToRefresh = false,
+        element = document.getElementById("scrollableBox");
 
-    // Pull-to-refresh will only trigger if the scroll begins when the
-    // document's Y offset is zero.
-
-    var touchstartHandler = function(e) {
-        if( e.touches.length != 1 ) {             
-            return ;
-        }
-        lastTouchY = e.touches[0].clientY ;
-        // maybePreventPullToRefresh = (preventPullToRefreshCheckbox.checked) && (window.pageYOffset == 0) ;
-        maybePreventPullToRefresh = (window.pageYOffset === 0) ;
-        //document.getElementById('txtLog').textContent = "maybePreventPullToRefresh: " + maybePreventPullToRefresh;
+    var touchstartHandler = function (e) {
+        maybePreventPullToRefresh = (element.scrollTop === 0) || (element.scrollTop >= (element.scrollHeight - element.offsetHeight));
+        log(maybePreventPullToRefresh);
     };
 
-    // To suppress pull-to-refresh it is sufficient to preventDefault the
-    // first overscrolling touchmove.
-
-    var touchmoveHandler = function(e) {
-        var touchY = e.touches[0].clientY ;
-        var touchYDelta = touchY - lastTouchY ;
-        lastTouchY = touchY ;
-
+    var touchmoveHandler = function (e) {
         if (maybePreventPullToRefresh) {
-            maybePreventPullToRefresh = false ;
-            if (touchYDelta >= 0) {
-                e.preventDefault() ;
-               log("pull-to-refresh event detected" + touchYDelta) ;
-                return ;
-            }
+            maybePreventPullToRefresh = false;
+            e.preventDefault();
+            return;
         }
-
-        // if (preventScrollCheckbox.checked) {
-        //     e.preventDefault() ;
-        //     return ;
-        // }
-
-        // if (preventOverscrollGlowCheckbox.checked) {
-        //     if (window.pageYOffset == 0 && touchYDelta > 0) {
-        //         e.preventDefault() ;
-        //         return ;
-        //     }
-        // }
     };
 
-    document.addEventListener('touchstart', touchstartHandler, false) ;
-    document.addEventListener('touchmove', touchmoveHandler, {passive: false}) ;
-}) ;
+    document.addEventListener('touchstart', touchstartHandler);
+    document.addEventListener('touchmove', touchmoveHandler, { passive: false });
+});
 
 // DO NOT DELETE
 function log(msg) {
     var p = document.getElementById('log');
     p.innerHTML = msg;
-  }
+}
