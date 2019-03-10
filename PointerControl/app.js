1da9a9b2a1768ecf7ad8f3b34c6455a1d69abdb8
Anderson <MDAnderson@samaritan.org>
2019-03-09 22:41:53 -0500

    na

--- PointerControl/app.js
+++ PointerControl/app.js
@@ -4,33 +4,35 @@ function log(msg) {
     p.innerHTML = msg;
 }
 
-// prevent chain scrolling
-var elem = document.getElementsByClassName("scrollable")[0],
-    lastTouchY;
+// prevent chain scrolling and pull-to-refresh
+for (var i = 0; i < document.getElementsByClassName("scrollable"); i++) {
+    var elem = document.getElementsByClassName("scrollable")[i],
+        lastTouchY;
 
-elem.addEventListener('touchstart', function (e) {
-    lastTouchY = e.changedTouches[0].clientY;
-    e.stopPropagation();
-});
+    elem.addEventListener('touchstart', function (e) {
+        lastTouchY = e.changedTouches[0].clientY;
+        e.stopPropagation();
+    });
 
-elem.addEventListener('touchmove', function (e) {
-    var touchY = e.changedTouches[0].clientY,
-        atTop = (elem.scrollTop === 0),
-        atBottom = (elem.scrollTop === (elem.scrollHeight - elem.offsetHeight));
-    
-    if (atTop && touchY > lastTouchY && e.cancelable) {
-        // stop scrolling up
-        e.preventDefault();
-        elem.scrollTop = 0;
-    }
-    if (atBottom && touchY < lastTouchY && e.cancelable) {
-        // stop scrolling down
-        e.preventDefault();
-        elem.scrollTop = (elem.scrollHeight - elem.offsetHeight);
-    }
-    e.stopPropagation();
-}, { passive: false });
+    elem.addEventListener('touchmove', function (e) {
+        var touchY = e.changedTouches[0].clientY,
+            atTop = (elem.scrollTop === 0),
+            atBottom = (elem.scrollTop === (elem.scrollHeight - elem.offsetHeight));
 
-elem.addEventListener('touchend', function (e) {
-    e.stopPropagation();
-});
+        if (atTop && touchY > lastTouchY && e.cancelable) {
+            // stop scrolling up
+            e.preventDefault();
+            elem.scrollTop = 0;
+        }
+        if (atBottom && touchY < lastTouchY && e.cancelable) {
+            // stop scrolling down
+            e.preventDefault();
+            elem.scrollTop = (elem.scrollHeight - elem.offsetHeight);
+        }
+        e.stopPropagation();
+    }, { passive: false });
+
+    elem.addEventListener('touchend', function (e) {
+        e.stopPropagation();
+    });
+}
