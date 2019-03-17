2171c084e2f533eab1c5ed99a033617b750a9ee3
Anderson <MDAnderson@samaritan.org>
2019-03-17 16:21:45 -0400

    added touchmove handler on window

--- scripts/app.js
+++ scripts/app.js
@@ -4,9 +4,29 @@ function toggleWidgetToolbar() {
     document.getElementById("nav-icon").classList.toggle("open");
 }
 
+// prevent all un-handled touchmove events
+window.addEventListener('touchstart', touchstartHandler);
+function touchstartHandler(e) {
+    window.addEventListener('touchmove', touchmoveHandler, { passive: false });
+    window.addEventListener('touchend', touchendHandler);
+    window.addEventListener('touchcancel', touchendHandler);
+}
+function touchmoveHandler(e) {
+    e.preventDefault();
+    e.stopPropagation();
+}
+function touchendHandler(e) {
+    window.removeEventListener('touchmove', touchmoveHandler);
+    window.removeEventListener('touchend', touchendHandler);
+    window.removeEventListener('touchcancel', touchendHandler);
+}
+
+// init draggable elements
 var el = document.getElementsByClassName('draggable')[0],
     options = {
         handle: el.children[0]
     };
  
 var drag = new Draggable(el, options);
+
+
