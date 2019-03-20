daee7a28d422b74d564ea8dcf08bb549e24ebe94
Anderson <MDAnderson@samaritan.org>
2019-03-20 16:32:08 -0400

    na

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -63,9 +63,20 @@
         function autoScroll() {
             // see: https://stackoverflow.com/questions/1810742/algorithm-to-implement-kinetic-scrolling
             // implement easing function
-            // (0.25, 0.1, 0.25, 1) ease function
+            // http://cubic-bezier.com/#.25,.1,.25,1
             // cubic-beizer: f(t) = a(1 - t)^3 + 3b(1 - t)^2 + 3c(1 - t)t^2 + dt^3
             console.log('v: ' + _velocity);
+ 
+            // P0 and P3 are anchors. P1Y = 0.1, P2Y = P3Y = 1
+            // B(t) = 3*(1-t)*(1-t)*t*P1Y + 3*(1-t)*t*t + t*t*t
+        }
+
+        function cubicBezier (t, p1, p2) {
+            return 3*p1*(1 - 2*t + t*t) + 3*p2*t*t*(1-t) + t*t*t;
+        }
+
+        function ease (t) {
+            return 0.3*t + 2.4*t*t - 1.7*t*t*t;
         }
 
         function copyTouch(touch) {
