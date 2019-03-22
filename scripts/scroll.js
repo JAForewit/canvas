b86ca07b105a7f431c9d31bfb8a03e509fbba124
Anderson <MDAnderson@samaritan.org>
2019-03-21 21:57:34 -0400

    na

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -7,91 +7,41 @@
         root.Scroll = factory();
     }
 }(this, function () {
+
     'use strict';
 
-    // TODO: catch scroll event while scrolling
+    // PRIVATE VARIABLES
 
+    // PRIVATE FUNCTIONS
+
+    /*
+    usage:
+    new Scroll (element, options)
+      - or -
+    new Scroll (element)
+    */
     function Scroll(element, options) {
         var me = this;
 
         me.el = element;
-        me.touch = {};
 
-        var _initialTouch = {},
-            _scrolling = false,
-            _offset,
-            _velocity,
-            _ticker;
+        var velocity = 40;
 
-        function touchstartHandler(e) {
-            if (_scrolling) return;
-            me.el.addEventListener('touchmove', touchmoveHandler);
-            me.el.addEventListener('touchend', touchendHandler);
-            me.el.addEventListener('touchcancel', touchendHandler);
-
-            me.touch = copyTouch(e.touches[0]);
-            _initialTouch = me.touch;
-            _scrolling = true;
-            _velocity = 0;
-            _offset = 0;
-            _ticker = requestAnimationFrame(track);
-        }
-
-        function touchmoveHandler(e) {
-            if (e.touches[0].identifier != _initialTouch.identifier) return;
-
-            var touch = copyTouch(e.touches[0]);
-            _offset = me.touch.y - touch.y;
-
-            me.el.scrollTo(0, me.el.scrollTop + _offset);
-            me.touch = touch;
-        }
-
-        function touchendHandler(e) {
-            if (e.changedTouches[0].identifier == _initialTouch.identifier
-                || e.touches.length == 0 || e.type == 'touchcancel') {
-
-                me.el.removeEventListener('touchmove', touchmoveHandler);
-                me.el.removeEventListener('touchend', touchendHandler);
-                me.el.removeEventListener('touchcancel', touchendHandler);
-
-                _scrolling = false;
-                cancelAnimationFrame(_ticker);
-
-                if (_velocity > 10 || _velocity < 10) {
-                    requestAnimationFrame(autoScroll)
-                }
-            }
-        }
-
+        requestAnimationFrame(autoScroll);
         function autoScroll() {
-            _velocity *= 0.9;
-
-            if (_velocity > 0.5 || _velocity < -0.5) {
-                me.el.scrollTo(0, me.el.scrollTop + _velocity);
-                requestAnimationFrame(autoScroll);
+            velocity *= 0.95;
+            console.log(velocity);
+            me.el.scrollTop += velocity;
+            if (velocity < 0.2) {
+                me.el.scrollTop = 0;
+                velocity = 40;
             }
+            requestAnimationFrame(autoScroll);
         }
 
-        function track() {
-            _velocity = 0.8 * _offset + 0.2 * _velocity;
-            console.log(_velocity);
-            if (_scrolling) requestAnimationFrame(track);
-        }
 
-        function cubicBezier(t, p1, p2) {
-            return 3 * p1 * (1 - 2 * t + t * t) + 3 * p2 * t * t * (1 - t) + t * t * t;
-        }
 
-        function ease(t) {
-            return 0.3 * t + 2.4 * t * t - 1.7 * t * t * t;
-        }
 
-        function copyTouch(touch) {
-            return { identifier: touch.identifier, x: touch.clientX, y: touch.clientY };
-        }
-
-        me.el.addEventListener('touchstart', touchstartHandler);
     }
 
     return Scroll;
