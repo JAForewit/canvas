8c1fd8ff7ef82b35f213e743c038970d837f1b63
Anderson <MDAnderson@samaritan.org>
2019-05-10 12:25:54 -0400

    created canvas.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,10 @@
-// FOR DEBUGGING
+//FOR DEBUGGING
 function log(msg) {
     var p = document.getElementById('log');
     p.innerHTML = msg;
 }
 
-// toggle for toolbars
+//setup toolbar toggles
 function toggleWidgetToolbar() {
     document.getElementById("widget-toolbar").classList.toggle("open");
     document.getElementById("nav-icon").classList.toggle("open");
@@ -14,16 +14,16 @@ function togglePrefToolbar() {
     document.getElementById("nav-icon2").classList.toggle("open");
 }
 
-// prevent all un-handled touchmove events
+//prevent all un-handled touchmove events
 document.body.addEventListener('touchmove', function (e) {
     e.preventDefault();
     e.stopPropagation();
 }, { passive: false });
 
-// init widget list with draggable elements
+//init widget list with draggable elements
 mylist = new WidgetList(document.getElementById("widget-toolbar"));
 
-// init scrollable elements
+//init scrollable elements
 for (var i = 0; i < document.getElementsByClassName('scrollable').length; i++) {
     var scrollEl = document.getElementsByClassName('scrollable')[i],
         options = {};
