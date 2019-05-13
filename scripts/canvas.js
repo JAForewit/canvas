92454a2df13c394d854e7c3ffe3eaa529cdee085
Anderson <MDAnderson@samaritan.org>
2019-05-13 15:53:40 -0400

    some cleanup

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -23,61 +23,101 @@
     function Canvas(canvas, options) {
         var me = this;
 
+
+        //TODO: move vars here
+        var selectedObject,
+            scene,
+            effectsScene,
+            camera,
+            directionalLight,
+            renderer,
+            preRenderFunctions,
+            raycaster,
+            mouse;
+
         //scenes
-        var scene = new THREE.Scene();
-        var effectsScene = new THREE.Scene();
+        scene = new THREE.Scene();
+        effectsScene = new THREE.Scene();
 
         //camera
-        var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+        camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
         camera.position.z = 5;
 
         //lights
-        var directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+        directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
         directionalLight.position.set(0, 0, 1);
         scene.add(directionalLight);
 
         //renderer
-        var renderer = new THREE.WebGLRenderer({
+        renderer = new THREE.WebGLRenderer({
             antialias: true,
             canvas: canvas,
             alpha: true
         });
         renderer.setSize(window.innerWidth, window.innerHeight);
         renderer.autoClear = false;
-        var preRenderFunctions = [];
+        preRenderFunctions = [];
+
+        //pointer control
+        mouse = {x:1, y:-1}; 
+        canvas.addEventListener('touchstart', startHandler, { passive: false });
+        canvas.addEventListener('mousedown', startHandler);
+        function startHandler(e) {
+            if (e.type === 'mousedown') {
+                canvas.addEventListener('mousemove', moveHandler, { passive: false });
+                canvas.addEventListener('mouseup', endHandler);
+                mouse = { x: e.clientX, y: e.clientY };
+            } else {
+                canvas.addEventListener('touchmove', moveHandler, { passive: false });
+                canvas.addEventListener('touchend', endHandler);
+                canvas.addEventListener('touchcancel', endHandler);
+                mouse = copyTouch(e.targetTouches[0]);
+                e.preventDefault();
+                e.stopPropagation();
+            }
+            //handle pointer start
+            selectedObject = checkIntersection();
+        }
+        function moveHandler(e) {
+            mouse = (e.type == 'mousemove')
+                ? { x: e.clientX, y: e.clientY }
+                : copyTouch(e.targetTouches[0]);
+
+            e.preventDefault();
+            e.stopPropagation();
+            //handle pointer move
+            selectedObject = checkIntersection();
+        }
+        function endHandler(e) {
+            if (e.type === 'mouseup') {
+                canvas.removeEventListener('mousemove', moveHandler);
+                canvas.removeEventListener('mouseup', endHandler);
+            } else if (e.targetTouches.length == 0 || e.targetTouches[0].identifier != me.pointer.identifier) {
+                canvas.removeEventListener('touchmove', moveHandler);
+                canvas.removeEventListener('touchend', endHandler);
+                canvas.removeEventListener('touchcancel', endHandler);
+            } else {
+                return;
+            }
+            //handle pointer end
+            selectedObject = false;
+        }
+        function copyTouch(touch) {
+            return { identifier: touch.identifier, x: touch.clientX, y: touch.clientY };
+        }
 
         //raycaster
-        var raycaster = new THREE.Raycaster();
-        var mouse = new THREE.Vector2(1,-1);
-        var touchedObject = null;
-
-        window.addEventListener('mousemove', onTouchMove);
-        window.addEventListener('touchmove', onTouchMove);
-
-        function onTouchMove(e) {
-
-            var x, y;
-            if (event.changedTouches) {
-                x = e.changedTouches[0].pageX;
-                y = e.changedTouches[0].pageY;
-            } else {
-                x = event.clientX;
-                y = event.clientY;
-            }
-            mouse.x = (x / window.innerWidth) * 2 - 1;
-            mouse.y = - (y / window.innerHeight) * 2 + 1;
-        }
+        raycaster = new THREE.Raycaster();
+        var clipMouse = new THREE.Vector2(1,-1);
         function checkIntersection() {
-            raycaster.setFromCamera(mouse, camera);
+            //tranlate to clipspace
+            clipMouse.x = (mouse.x / window.innerWidth) * 2 - 1;
+            clipMouse.y = -(mouse.y / window.innerHeight) * 2 + 1;
+
+            raycaster.setFromCamera(clipMouse, camera);
             var intersects = raycaster.intersectObjects([scene], true);
-            if (intersects.length > 0) {
-                var selectedObject = intersects[0].object;
-                touchedObject = selectedObject;
-            } else if (touchedObject != null) {
-                touchedObject = null;
-            }
+            return (intersects.length > 0) ? intersects[0].object : false;
         }
-        preRenderFunctions.push(checkIntersection);
 
         //window resize
         window.addEventListener('resize', onWindowResize, false);
@@ -90,32 +130,39 @@
         }
 
         //geometry
-        var geometry = new THREE.BoxGeometry(1, 1, 1);
+        var geometry = new THREE.CubeGeometry( 1, 1, 1 );
         var material = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
         var cube = new THREE.Mesh(geometry, material);
         scene.add(cube);
 
         //outline object
-        //CURRENTLY: outline any item hovered over
         var outlineMat = new THREE.MeshBasicMaterial({color: 0xff0000, side: THREE.BackSide});
-        var outlineMesh = new THREE.Mesh(cube.geometry, outlineMat);
-        outlineMesh.scale.multiplyScalar(1.1);
+        var outlineGeo = new THREE.Geometry();
+        var outline = new THREE.Mesh(outlineGeo, outlineMat);
+        outline.scale.multiplyScalar(1.1);
         function updateOutlines() {
-            if (touchedObject) {
-                effectsScene.add(outlineMesh);
-                outlineMesh.rotation.set(
-                    touchedObject.rotation.x, 
-                    touchedObject.rotation.y, 
-                    touchedObject.rotation.z
+            //TODO: change to "for i in selectedObjects"
+            if (selectedObject) {
+                outline.geometry = selectedObject.geometry;
+                effectsScene.add(outline);
+                outline.rotation.set(
+                    selectedObject.rotation.x, 
+                    selectedObject.rotation.y, 
+                    selectedObject.rotation.z
                 );
-            } else if (outlineMesh.parent === effectsScene) {
-                effectsScene.remove(outlineMesh);
+                outline.position.set(
+                    selectedObject.position.x,
+                    selectedObject.position.y,
+                    selectedObject.position.z
+                );
+            } else {
+                effectsScene.remove(outline);
             }
         }
         preRenderFunctions.push(updateOutlines);
 
         //render loop
-        function animate() {
+        function animate(time) {
             requestAnimationFrame(animate);
 
             cube.rotation.z += 0.01;
@@ -128,6 +175,14 @@
             renderer.render(scene, camera);
         }
         animate();
+
+
+
+
+        
+
+
+
     } 
 
     return Canvas;
